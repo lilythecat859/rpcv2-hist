c8dc96dc7a287b8d5f23b53938ce2ea79d804a44
lilythecat859 <lilythecat859@gmail.com>
2025-10-21 05:11:57 -0400

    Create terraform/variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,22 @@
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "us-central1-a"
+}
+
+variable "machine_type" {
+  description = "Machine type for the instance"
+  type        = string
+  default     = "n2d-standard-16"
+}
